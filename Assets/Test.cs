dbee09676db8ceac49c945f98da800db60ddd55e
Ango2210 <hansol920273@gmail.com>
2025-10-22 12:38:39 +0900

    수정

--- Assets/Test.cs
+++ Assets/Test.cs
@@ -2,13 +2,12 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class HelloWorld : MonoBehaviour
+public class Test : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
     {
-        Debug.Log("Hello World");
-        Debug.Log("æ»≥Á«œººø‰");
+        Debug.Log("test");
     }
 
     // Update is called once per frame
